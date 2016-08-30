69a29f5ee9af52d7cc025215e1743b489de3e99c
ghornich <hornichg@gmail.com>
2016-08-30 18:22:58 +0200

    Add update-source-version gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ var fs = require('fs');
 var gulp = require('gulp');
 var browserify = require('browserify');
 var markdownTOC = require('markdown-toc');
+var packageJson = require('./package.json');
 
-gulp.task('pre-publish', ['build-browser', 'update-readme-toc']);
+gulp.task('pre-publish', ['update-source-version', 'build-browser', 'update-readme-toc']);
 gulp.task('build-browser', ['build-browser-full', 'build-browser-min']);
 
 gulp.task('build-browser-full', function () {
@@ -27,6 +28,17 @@ gulp.task('update-readme-toc', function () {
     var readme = fs.readFileSync('README.md', { encoding: 'utf-8' });
     readme = markdownTOC.insert(readme);
     fs.writeFileSync('README.md', readme, { encoding: 'utf-8' });
+});
+
+gulp.task('update-source-version', function () {
+    var source = fs.readFileSync('split-retain.js', { encoding: 'utf-8' });
+    var newSource = source.replace(/(splitRetain\['VERSION'\] = ')[^']+/, '$1' + packageJson.version);
+
+    if (source === newSource) {
+        throw new Error('source version change failed: version didn\'t change');
+    }
+
+    fs.writeFileSync('split-retain.js', newSource, { encoding: 'utf-8' });
 });
 
 function handleStreamError(error) {
