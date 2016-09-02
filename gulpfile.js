dac94b6d246a22d52293adb315bb9c2e3d017f4b
ghornich <hornichg@gmail.com>
2016-09-02 12:25:31 +0200

    Migrate remaining tasks to gulp-publish-tasks, remove deps

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,11 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var packageJson = require('./package.json');
 var version = packageJson.version;
-var publishTasks = require('gulp-publish-tasks');
+var publishTasks = require('../gulp-publish-tasks');
 
+gulp.task('pre-publish', ['update-source-version', 'build-browser-full', 'build-browser-min', 'update-readme-toc']);
 
-
-gulp.task('pre-publish', ['update-source-version', 'build-browser', 'update-readme-toc']);
-
-gulp.task('build-browser', ['build-browser-full', 'build-browser-min']);
 gulp.task('update-readme-toc', () => publishTasks.updateMarkdownTOC('README.md'));
 gulp.task('update-source-version', () => publishTasks.updateSourceVersion('split-retain.js', version));
-
-
-
-gulp.task('build-browser-full', function () {
-    return browserify('browser-build.js')
-    .bundle()
-    .on('error', handleStreamError)
-    .pipe(fs.createWriteStream('browser/split-retain.js'))
-});
-
-gulp.task('build-browser-min', function () {
-    return browserify('browser-build.js')
-    .plugin('minifyify', { map: false })
-    .bundle()
-    .on('error', handleStreamError)
-    .pipe(fs.createWriteStream('browser/split-retain.min.js'));
-});
-
-function handleStreamError(error) {
-    console.error(error.stack);
-    this.emit('end');
-}
+gulp.task('build-browser-full', () => publishTasks.browserify('browser-build.js', 'browser/split-retain.js'));
+gulp.task('build-browser-min', () => publishTasks.browserify('browser-build.js', 'browser/split-retain.min.js', { minify: true }));
